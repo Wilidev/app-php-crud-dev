731a0ea8c98a3fc099b233c965247a5a5dd234e6
WiliamDida1 <wiliamccanccapaquiro@gmail.com>
2025-11-17 14:45:19 -0500

    nuevos cambios de trabajo

---
+++ login.php
@@ -0,0 +1,52 @@
+<?php
+session_start();
+require 'config/db.php';
+include 'includes/header.php';
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $correo = $_POST['correo'];
+    $password = $_POST['password'];
+
+    // var_dump($correo, $password);
+
+    $stmt = $pdo->prepare("SELECT * FROM usuarios where usuarios = ?");
+    $stmt->execute([$correo]);
+    $usuario = $stmt ->fetch(PDO::FETCH_ASSOC);
+    
+    if ($usuario && $password == $usuario['password']) {
+
+        $_SESSION['id_usuarios'] = $usuario['id_usuarios'];
+        $_SESSION['nombre'] = 'Wiliam Dida';
+        $_SESSION['rol'] = $usuario['rol'];
+
+        header('location: index.php');
+
+        echo "Excelente ingresaste con exito.🍀🍀🍀";
+    }else{
+        echo "Usuario o contraseña Incorrecta. 🤣🤣";
+    }
+
+}
+?>
+<h2>Login 🍀</h2>
+
+<form method="POST">
+    <div class="mb-3">
+        <label for="correo" class="form-label">Correo</label>
+        <input type="email" class="form-control" id="correo" name="correo" required>
+    </div>
+
+    <div class="mb-3">
+        <label for="password" class="form-label">Contraseña</label>
+        <input type="password" class="form-control" id="password" name="password" required>
+    </div>
+
+    <button type="submit" class="btn btn-primary">Ingresar</button>
+</form>
+
+<?php
+include 'includes/footer.php';
+
+?>
